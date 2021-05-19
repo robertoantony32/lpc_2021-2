0f1b107637465d28a4b470a51ffd64f728079b12
Roberto Antônio <74279517+robertoantony32@users.noreply.github.com>
2021-05-19 20:36:57 -0300

    Mudanças na formatação do código de forma geral, e resolução do bug da raquete.

--- pong2/mypongpygame.py
+++ pong2/mypongpygame.py
@@ -1,166 +1,243 @@
 import pygame
+from pygame.locals import *
+from random import choice
 
 pygame.init()
 
-COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE = (255, 255, 255)
+# screen dimensions
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-SCORE_MAX = 2
 
-size = (1280, 720)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("MyPong - PyGame Edition - 2021.01.30")
+# player object
+class Player:
+    # function for player dimensions
+    def __init__(self, x, y):
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(x, y)
+
+        # movement of the player
+        self.up = False
+        self.down = False
+
+        self.score = 00
+
+    # function for the movement of the player
+    def movement(self):
+        if self.up:
+            self.rect.y -= 5
+        elif self.down:
+            self.rect.y += 5
+
+    # player colliding with limits
+    def is_colliding_with_limits(self):
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.y = 570
+        elif self.rect.top <= 0:
+            self.rect.y = 0
+
+    # function that calls the other functions previously programmed
+    def update(self):
+        self.movement()
+        self.is_colliding_with_limits()
+
+    # function for drawing the player
+    def render(self, screen: pygame.surface):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+# bot object
+class Bot:
+    # bot stats
+    def __init__(self, x, y):
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(x, y)
+
+        self.score = 00
+
+    # bot movement
+    def bot_movement(self):
+        self.rect.y = ball.rect.y
+
+    # bot colliding with limits
+    def is_colliding_with_limits(self):
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.y = 570
+        elif self.rect.top <= 0:
+            self.rect.y = 0
+
+    # function that calls the other functions previously programmed
+    def update(self):
+        self.bot_movement()
+        self.is_colliding_with_limits()
+
+    # function to draw the bot paddle
+    def render(self, screen: pygame.surface):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+# ball object
+class Ball:
+    # Ball structure function
+    def __init__(self, x, y):
+        # dimensions
+        self.width = 20
+        self.height = 20
+
+        # ball direction
+        self.dy = 1
+        self.dx = 1
+
+        # speed of the ball
+        self.SPEED = 5
+
+        self.rect = pygame.Rect(x, y, 20, 20)
+
+    #  colliding function
+    def is_colliding_with_paddle(self):
+        #  colliding with paddle 1 verification
+        if self.rect.colliderect(player.rect) and self.dx < 0:
+            self.dx *= -1
+
+            # setting the bounce sound
+            pygame.mixer.Sound('assets/bounce.wav').play()
+
+            # reaction of the ball when it touches the bottom of the paddle 1
+            if player.rect.bottom >= self.rect.top > (player.rect.centery + 20):
+                self.dy = 1
+
+            # reaction of the ball when it touches the top of the paddle 1
+            elif player.rect.top <= self.rect.bottom < (player.rect.centery - 20):
+                self.dy = -1
+
+            # reaction of the ball when it touches the middle of the paddle 1
+            elif (player.rect.centery + 20) >= self.rect.centery > (player.rect.centery - 20):
+                self.dy = 0
+
+        # colliding with paddle 2 verification
+        elif self.rect.colliderect(bot.rect) and self.dx > 0:
+            self.dx *= -1
+
+            # setting the bounce sound
+            pygame.mixer.Sound('assets/bounce.wav').play()
+
+            # reaction of the ball when it touches the bottom of the paddle 2
+            if bot.rect.bottom >= self.rect.top > (bot.rect.centery - 20):
+                self.dy = 1
+
+            # reaction of the ball when it touches the top of the paddle 2
+            elif bot.rect.top <= self.rect.bottom < (bot.rect.centery - 20):
+                self.dy = -1
+
+            # reaction of the ball when it touches the middle of the paddle 2
+            elif (bot.rect.centery + 20) >= self.rect.centery > (bot.rect.centery - 20):
+                self.dy = 0
+
+    # check the collision of the with boundaries of the screen
+    def is_colliding_with_limits(self):
+        if self.rect.top >= WINDOW_HEIGHT or self.rect.bottom <= 0:
+            self.dy *= -1
+            pygame.mixer.Sound('assets/bounce.wav').play()
+
+        # setting the player score
+        elif self.rect.right >= WINDOW_WIDTH:
+            pygame.mixer.Sound('assets/258020__kodack__arcade-bleep-sound.wav').play()
+            self.rect.x = 640
+            self.rect.y = 360
+            self.dx *= -1
+            player.score += 1
+            self.dy = choice([1, -1])
+
+        # setting the bot score
+        elif self.rect.left <= 0:
+            pygame.mixer.Sound('assets/258020__kodack__arcade-bleep-sound.wav').play()
+            self.rect.x = 640
+            self.rect.y = 360
+            self.dx *= -1
+            bot.score += 1
+            self.dy = choice([1, -1])
+
+    # function that calls the other functions previously programmed
+    def update(self):
+        self.rect.y += self.dy * self.SPEED
+        self.rect.x += self.dx * self.SPEED
+        self.is_colliding_with_limits()
+        self.is_colliding_with_paddle()
+
+    # function to draw the ball
+    def render(self, screen: pygame.surface):
+        pygame.draw.ellipse(screen, (255, 255, 255), self.rect)
+
+
+# setting the screen
+Screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("MyPong - Pygame by Roberto.")
+
+# setting the fps
+clock = pygame.time.Clock()
+
+# setting the objects
+player = Player(50, 300)
+ball = Ball(640, 360)
+bot = Bot(1180, 300)
 
 # score text
 score_font = pygame.font.Font('assets/PressStart2P.ttf', 44)
-score_text = score_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
+score_text = score_font.render('00 x 00', True, (255, 255, 255))
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (680, 50)
 
 # victory text
 victory_font = pygame.font.Font('assets/PressStart2P.ttf', 100)
-victory_text = victory_font .render('VICTORY', True, COLOR_WHITE, COLOR_BLACK)
+victory_text = victory_font .render('VICTORY', True, (255, 255, 255), (0, 0, 0))
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (450, 350)
 
-# sound effects
-bounce_sound_effect = pygame.mixer.Sound('assets/bounce.wav')
-scoring_sound_effect = pygame.mixer.Sound('assets/258020__kodack__arcade-bleep-sound.wav')
+is_run = True
 
-# player 1
-player_1 = pygame.image.load("assets/player.png")
-player_1_y = 300
-player_1_move_up = False
-player_1_move_down = False
+# screen loop
+while is_run:
+    # fps
+    clock.tick(60)
 
-# player 2 - robot
-player_2 = pygame.image.load("assets/player.png")
-player_2_y = 300
-
-# ball
-ball = pygame.image.load("assets/ball.png")
-ball_x = 640
-ball_y = 360
-ball_dx = 5
-ball_dy = 5
-
-# score
-score_1 = 0
-score_2 = 0
-
-# game loop
-game_loop = True
-game_clock = pygame.time.Clock()
-
-while game_loop:
-
+    # verification to pick up interactions with the keyboard or mouse
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_loop = False
+        # defining a way out of the game
+        if event.type == QUIT:
+            is_run = False
 
-        #  keystroke events
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                player_1_move_up = True
-            if event.key == pygame.K_DOWN:
-                player_1_move_down = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                player_1_move_up = False
-            if event.key == pygame.K_DOWN:
-                player_1_move_down = False
+        # checking for when the key is pressed
+        elif event.type == KEYDOWN:
+            # setting the player movement
+            if event.key == K_UP:
+                player.up = True
+            elif event.key == K_DOWN:
+                player.down = True
+        # checking when the key is released
+        elif event.type == KEYUP:
+            # setting the player movement
+            if event.key == K_UP:
+                player.up = False
+            elif event.key == K_DOWN:
+                player.down = False
 
-    # checking the victory condition
-    if score_1 < SCORE_MAX and score_2 < SCORE_MAX:
+    # update the score
+    score_text = score_font.render(str(player.score) + "x" + str(bot.score),
+                                   True, (255, 255, 255))
+    if player.score < 1 and bot.score < 1:
+        player.update()
+        ball.update()
+        bot.update()
+        Screen.fill((0, 0, 0))
+        player.render(Screen)
+        ball.render(Screen)
+        bot.render(Screen)
+        Screen.blit(score_text, score_text_rect)
 
-        # clear screen
-        screen.fill(COLOR_BLACK)
-
-        # ball collision with the wall
-        if ball_y > 700:
-            ball_dy *= -1
-            bounce_sound_effect.play()
-        elif ball_y <= 0:
-            ball_dy *= -1
-            bounce_sound_effect.play()
-
-        # ball collision with the player 1 's paddle
-        if ball_x < 100:
-            if player_1_y < ball_y + 25:
-                if player_1_y + 150 > ball_y:
-                    ball_dx *= -1
-                    bounce_sound_effect.play()
-
-        # ball collision with the player 2 's paddle
-        if ball_x > 1160:
-            if player_2_y < ball_y + 25:
-                if player_2_y + 150 > ball_y:
-                    ball_dx *= -1
-                    bounce_sound_effect.play()
-
-        # scoring points
-        if ball_x < -50:
-            ball_x = 640
-            ball_y = 360
-            ball_dy *= -1
-            ball_dx *= -1
-            score_2 += 1
-            scoring_sound_effect.play()
-        elif ball_x > 1320:
-            ball_x = 640
-            ball_y = 360
-            ball_dy *= -1
-            ball_dx *= -1
-            score_1 += 1
-            scoring_sound_effect.play()
-
-        # ball movement
-        ball_x = ball_x + ball_dx
-        ball_y = ball_y + ball_dy
-
-        # player 1 up movement
-        if player_1_move_up:
-            player_1_y -= 5
-        else:
-            player_1_y += 0
-
-        # player 1 down movement
-        if player_1_move_down:
-            player_1_y += 5
-        else:
-            player_1_y += 0
-
-        # player 1 collides with upper wall
-        if player_1_y <= 0:
-            player_1_y = 0
-
-        # player 1 collides with lower wall
-        elif player_1_y >= 570:
-            player_1_y = 570
-
-        # player 2 "Artificial Intelligence"
-        player_2_y = ball_y
-        if player_2_y <= 0:
-            player_2_y = 0
-        elif player_2_y >= 570:
-            player_2_y = 570
-
-        # update score hud
-        score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
-
-        # drawing objects
-        screen.blit(ball, (ball_x, ball_y))
-        screen.blit(player_1, (50, player_1_y))
-        screen.blit(player_2, (1180, player_2_y))
-        screen.blit(score_text, score_text_rect)
     else:
         # drawing victory
-        screen.fill(COLOR_BLACK)
-        screen.blit(score_text, score_text_rect)
-        screen.blit(victory_text, victory_text_rect)
+        Screen.fill((0, 0, 0))
+        Screen.blit(score_text, score_text_rect)
+        Screen.blit(victory_text, victory_text_rect)
 
-    # update screen
     pygame.display.flip()
-    game_clock.tick(60)
-
-pygame.quit()
